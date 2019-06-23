fb4fede5f02f29779146cb093cc0d801d7db1215
kurozumi <info@a-zumi.net>
2019-06-23 21:24:37 +0900

    update

---
+++ tests/ApiTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ApiTest extends TestCase
+{
+
+    public function testSomething()
+    {
+        $this->assertTrue(true);
+    }
+}
